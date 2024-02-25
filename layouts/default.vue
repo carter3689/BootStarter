f36f687c5ffa899eb3400bfda2b1222362b707ad
Joel <carter3689@gmail.com>
2024-02-25 09:47:42 -0600

    Update: Adding to index structure

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
   <div class="col-start-1 col-span-1"></div>
 
   <!-- Center the heading in the second column -->
-  <h2 class="col-start-2 col-span-1 row-start-1 row-span-1 flex items-center justify-center text-3vw md:text-4vw lg:text-5vw xl:text-6vw 2xl:text-7vw">
+  <h2 class="font-Inter col-start-2 col-span-1 row-start-1 row-span-1 flex items-center justify-center text-3vw md:text-4vw lg:text-5vw xl:text-6vw 2xl:text-7vw">
   Bring A Creative Project To Life
   </h2>
 
@@ -24,4 +24,6 @@
   <!-- Empty cell for the third column -->
   <div class="col-start-3 col-span-1"></div>
 </div>
+<AppStats />
+<AppFeature />
   </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,11 +7,31 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     '@tailwindcss/forms',
+    '@nuxtjs/google-fonts'
   ],
   components: [
     {
       path: '~/components',
       pathPrefix: false
     }
-  ]
+  ],
+  googleFonts: {
+    inject: true,
+    base64: true,
+    useStylesheet: true,
+    families: {
+      Roboto: true,
+      'Josefin+Sans': true,
+      Lato: [100, 300],
+      Raleway: {
+        wght: [100, 400],
+        ital: [100]
+      },
+      Inter: '200..700',
+      'Crimson Pro': {
+        wght: '200..900',
+        ital: '200..700',
+      }
+    }
+  }
 })
